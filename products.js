be3c08c89f641beebcafeeb35438cacd487373f1
hayleamlak <hayleamlaktesfaye12@gmail.com>
2024-11-24 14:18:12 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,9 +1,9 @@
-constant products = [
+const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    ratingggg: {
+    rating: {
       stars: 4.5,
       count: 87
     },
